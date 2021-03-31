96854b6a961365a5eb71613743913cc0a49e8fbf
NafaaCHEMIL <nafaa.chemil@gmail.com>
2021-03-31 21:51:53 +0200

    chicken party in three

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,34 @@
 <template>
-	<ChickenCard notreMessage="Hello Friends" />
+	<div id="app">
+		<HelloWorld />
+	</div>
 </template>
 
 <script>
-import ChickenCard from "./components/ChickenCard.vue";
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
 	name: "App",
 	components: {
-		ChickenCard,
+		HelloWorld,
 	},
 };
 </script>
 
 <style>
+html,
+body,
+#app {
+	margin: 0;
+	padding: 0;
+	height: 100%;
+	width: 100%;
+}
 #app {
 	font-family: Avenir, Helvetica, Arial, sans-serif;
 	-webkit-font-smoothing: antialiased;
 	-moz-osx-font-smoothing: grayscale;
 	text-align: center;
 	color: #2c3e50;
-	margin-top: 60px;
 }
 </style>
