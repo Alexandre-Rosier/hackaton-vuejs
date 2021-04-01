dcc421c3b17edd75c7f8b555a1e6d3576b2ac20a
oliviermod8 <oliviermod8@gmail.com>
2021-04-01 18:15:08 +0200

    chicken sound

--- src/components/ChickenDescription.vue
+++ src/components/ChickenDescription.vue
@@ -2,15 +2,14 @@
 <div class="cardDetail">
     <div class="title">
     <img class="imgCard" src= '../assets/poulet.gif' />
-    <button @click.prevent="playSound('https://www.fesliyanstudios.com/play-mp3/6512')" >
+    <button @click.prevent="playSound('https://www.fesliyanstudios.com/play-mp3/6512')">
     <div class="pronom">la</div>
     <div class="h1Name">POULE</div>
     </button>
-    
     </div>
     <p class='text'>La poule est la femelle de l'espèce domestique des <span>gallinacés</span>.<br> Le mâle est le <span>coq</span>. Originaire d'Asie, il existe aujourd'hui de très nombreuses races de poules partout dans le monde.</p>
 </div>  
-</template>
+
 
 <script>
 export default {
